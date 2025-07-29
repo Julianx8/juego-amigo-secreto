88460bbaa8b2e9a6a0545dc158713e5d970bf955
Julian Betancur <betancuribarraj@gmail.com>
2025-07-29 18:15:58 -0500

    se agrego la lista

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('nombreIngresado').value;
     document.getElementById('nombreIngresado').valua ='';
+
+    if (nombreAmigo === '') {
+        alert('Por favor, Inserte un nombre');
+        return;
+    } else {
+         amigos.push(nombreAmigo);
+         
+
+}
     console.log(nombreAmigo);
+    console.log(amigos);
 }
 
 agregarAmigo();
